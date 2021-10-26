7ce54daab75548e30f41f6757116f9be05f88809
xevvvw <xevvvw@students.cs.ubc.ca>
2021-10-25 22:38:10 -0700

    update helper functions

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,4 +1,8 @@
 import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFoundError} from "./IInsightFacade";
+import * as fs from "fs-extra";
+import * as JSZip from "jszip";
+import * as checkHelper from "./checkHelperFunctions";
+import * as matchHelper from "./matchHelperFunctions";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -6,23 +10,192 @@ import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFou
  *
  */
 export default class InsightFacade implements IInsightFacade {
+	public insightDatasets: InsightDataset[];
+	public datasetIDList: string[];
+
 	constructor() {
 		console.trace("InsightFacadeImpl::init()");
+		this.insightDatasets = [];
+		this.datasetIDList = [];
+		// initiate the folder to store data
+		if (!fs.existsSync("./data/")) {
+			fs.mkdirSync("./data/");
+		}
+		let thisObject: this;
+		thisObject = this;
+		let files = fs.readdirSync("./data/");
+		files.forEach(function (fileName) {
+			thisObject.datasetIDList.push(fileName);
+			let data;
+			let currentDataset: InsightDataset;
+			let id: string = fileName;
+			let numOfRows: number;
+			numOfRows = 0;
+			try {
+				const content = fs.readFileSync("./data/" + fileName, "utf8");
+				data = JSON.parse(content);
+				numOfRows = data.length;
+				currentDataset = {id, kind: InsightDatasetKind.Courses, numRows: numOfRows};
+				thisObject.insightDatasets.push(currentDataset);
+			} catch (err) {
+				return Promise.reject(new InsightError("files in data dir reading error"));
+			}
+		});
+		console.log("This the loaded datasetIDList in the InsightFacade Constructor: ");
+		console.log(thisObject.datasetIDList);
+		console.log("");
+		console.log("This the loaded insightDatasets in the InsightFacade Constructor: ");
+		console.log(thisObject.insightDatasets);
 	}
 
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		return Promise.reject("Not implemented.");
+		let thisObject: this;
+		thisObject = this;
+		let coursesPromisesList: any[] = [];
+		checkHelper.setTheStoredIDList(thisObject.datasetIDList);
+		return new Promise(function (resolve, reject) {
+			if (!checkHelper.isIDValid(id)) {
+				return reject(new InsightError("invalid ID"));
+			}
+			if (kind === InsightDatasetKind.Courses) {
+				JSZip.loadAsync(content, {base64: true}).then((zipObj: any) => {
+					const coursesFolder = zipObj.folder("courses");
+					coursesFolder.forEach((relativePath: string, file: any) => {
+						const currentCoursePromise = file.async("string");
+						coursesPromisesList.push(currentCoursePromise);
+					});
+					let sectionsListToStore: any[] = [];
+					let numOfRows: number = 0;
+					Promise.all(coursesPromisesList).then((coursesList: any) => {
+						if (coursesList.length === 0) {
+							return reject(new InsightError("no course file in the courses folder"));
+						}
+						for (const course of coursesList) {
+							try {
+								let currentCourseSectionsList = JSON.parse(course)["result"];
+								if (currentCourseSectionsList.length > 0) {
+									for (const section of currentCourseSectionsList) {
+										sectionsListToStore.push(checkHelper.makeSectionToStore(id, section));
+										numOfRows += 1;
+									}
+								}
+							} catch (e) {
+								return reject(new InsightError("try adding sections error"));
+							}
+						}
+						if (sectionsListToStore.length === 0) {
+							return reject(new InsightError("the sections list to be stored is empty"));
+						} else {
+							thisObject.addDatasetHelper(id, sectionsListToStore, numOfRows);
+							return resolve(thisObject.datasetIDList);
+						}
+					});
+				});
+			} else {
+				reject(new InsightError("invalid kind, should be Course only"));
+			}
+		});
 	}
+
+	// 			}).catch(function (error: any) {
+	// 				return reject(new InsightError(error));
+	// 			});
+	// 		}).catch(function (error: any) {
+	// 			return reject(new InsightError(error));
+	// 		});
+	// 	}  else {
+	// 		return reject(new InsightError("invalid kind, should be Course only"));
+	// 	}
+	// });
 
 	public removeDataset(id: string): Promise<string> {
-		return Promise.reject("Not implemented.");
-	}
-
-	public performQuery(query: any): Promise<any[]> {
-		return Promise.reject("Not implemented.");
+		let thisObject: this;
+		thisObject = this;
+		let path: string = "./data/" + id;
+		return new Promise((resolve, reject) => {
+			if (id === null) {
+				return reject(new InsightError("ID is null"));
+			}
+			if (id.includes("_")) {
+				return reject(new InsightError("ID contains an underscore"));
+			}
+			if (!id.trim()) {
+				return reject(new InsightError("ID contains only white spaces"));
+			}
+			if (fs.existsSync(path)) {
+				fs.unlink(path, (err2) => {
+					if (err2) {
+						return reject(new InsightError("unexpected error"));
+					} else {
+						// remove id from datasetIDList
+						thisObject.datasetIDList.forEach((currentID, index) => {
+							if (currentID === id) {
+								delete thisObject.datasetIDList[index];
+							}
+						});
+						// remove insightDataset from insightDatasets
+						thisObject.insightDatasets.forEach((currentInsightDataset, index) => {
+							if (currentInsightDataset.id === id) {
+								delete thisObject.datasetIDList[index];
+							}
+						});
+						return resolve(id);
+					}
+				});
+			} else {
+				return reject(new NotFoundError("remove file/id not exist"));
+			}
+		});
 	}
 
 	public listDatasets(): Promise<InsightDataset[]> {
-		return Promise.reject("Not implemented.");
+		let thisObject: this;
+		thisObject = this;
+		return new Promise<InsightDataset[]>(function (resolve) {
+			return resolve(thisObject.insightDatasets);
+		});
+	}
+
+	public performQuery(query: any): Promise<any[]> {
+		let thisObject: this;
+		thisObject = this;
+		checkHelper.setTheStoredIDList(thisObject.datasetIDList);
+		return new Promise((resolve, reject) => {
+			if (checkHelper.checkQUERYValidOrNot(query)) {
+				let theFILTERInWHERE: any = query["WHERE"];
+				let options: any = query["OPTIONS"];
+				let columns: any[] = options["COLUMNS"];
+				let order: string = "";
+				if (Object.keys(options).includes("ORDER")) {
+					order = options["ORDER"];
+				}
+				let data: any[] = [];
+				try {
+					const content = fs.readFileSync("./data/" + checkHelper.currentReferencingDatasetID, "utf8");
+					data = JSON.parse(content);
+				} catch (err) {
+					return reject(new InsightError("file reading error"));
+				}
+				let matchedResult: any[];
+				let resultFilteredColumns: any[];
+				matchedResult = matchHelper.matchFILTER(data, theFILTERInWHERE);
+				if (matchedResult.length > 5000) {
+					return reject(new InsightError("Only queries with a maximum of 5000 results are supported."));
+				} else {
+					resultFilteredColumns = checkHelper.filterTheCOLUMNS(matchedResult, columns, order);
+					return resolve(resultFilteredColumns);
+				}
+			} else {
+				return reject(new InsightError("This query is not valid"));
+			}
+		});
+	}
+
+	public addDatasetHelper(id: string, sectionsListToStore: any[], numOfRows: number) {
+		let currentDataset: InsightDataset = {id, kind: InsightDatasetKind.Courses, numRows: numOfRows};
+		fs.writeFileSync("./data/" + id, JSON.stringify(sectionsListToStore), "utf8");
+		this.insightDatasets.push(currentDataset);
+		this.datasetIDList.push(id);
+		console.log("dataset: " + id + " added successfully");
 	}
 }
---
+++ src/controller/matchHelperFunctions.ts
@@ -0,0 +1,162 @@
+export function matchFILTER(data: any[], thisFilter: any): any[] {
+	console.log("now matching FILTER");
+	let matchedResult: any[] = [];
+	if (Object.keys(thisFilter).length === 0) {
+		console.log("this filter is empty, so this can only be the filter in WHERE, so all result matched");
+		matchedResult = data;
+	} else {
+		let theOnlyKeyInFILTER: any = Object.keys(thisFilter)[0];
+
+		if (theOnlyKeyInFILTER === "AND" || theOnlyKeyInFILTER === "OR") {
+			let TheLOGICCOMPARISON: any[] = thisFilter[theOnlyKeyInFILTER];
+			matchedResult = matchLogicComparison(data, theOnlyKeyInFILTER, TheLOGICCOMPARISON);
+		}
+
+		if (theOnlyKeyInFILTER === "GT" || theOnlyKeyInFILTER === "LT" || theOnlyKeyInFILTER === "EQ") {
+			let theMCOMPARISON: any = thisFilter[theOnlyKeyInFILTER];
+			matchedResult = matchMComparison(data, theOnlyKeyInFILTER, theMCOMPARISON);
+		}
+
+		if (theOnlyKeyInFILTER === "IS") {
+			let theSCOMPARISON: any = thisFilter[theOnlyKeyInFILTER];
+			matchedResult = matchSComparison(data, theSCOMPARISON);
+		}
+
+		if (theOnlyKeyInFILTER === "NOT") {
+			let theNEGATION: any = thisFilter[theOnlyKeyInFILTER];
+			matchedResult = matchNegation(data, theNEGATION);
+		}
+	}
+	return matchedResult;
+}
+function matchLogicComparison(data: any[], TheLOGICCOMPARISONKey: string, TheLOGICCOMPARISON: any[]): any[] {
+	let matchedResult: any[] = [];
+	if (TheLOGICCOMPARISONKey === "AND") {
+		matchedResult = data;
+		for (let eachFILTER of TheLOGICCOMPARISON) {
+			matchedResult = matchFILTER(matchedResult, eachFILTER);
+		}
+		return matchedResult;
+	} else {
+		matchedResult = [];
+		for (let eachFILTER of TheLOGICCOMPARISON) {
+			let currentResultOfEachFILTER: any[] = matchFILTER(data, eachFILTER);
+			for (let eachResult of currentResultOfEachFILTER) {
+				if (!matchedResult.includes(eachResult)) {
+					matchedResult.push(eachResult);
+				}
+			}
+		}
+		return matchedResult;
+	}
+}
+function matchMComparison(data: any[], TheMCOMPARISONKey: string, theMCOMPARISON: any): any[] {
+	let matchedResult: any[] = [];
+	let theMKey = Object.keys(theMCOMPARISON)[0];
+	let theMValue: number = theMCOMPARISON[theMKey];
+
+	if (TheMCOMPARISONKey === "GT") {
+		for (let eachSection of data) {
+			if (eachSection[theMKey] > theMValue) {
+				matchedResult.push(eachSection);
+			}
+		}
+		return matchedResult;
+	} else if (TheMCOMPARISONKey === "LT") {
+		for (let eachSection of data) {
+			if (eachSection[theMKey] < theMValue) {
+				matchedResult.push(eachSection);
+			}
+		}
+		return matchedResult;
+	} else {
+		for (let eachSection of data) {
+			if (eachSection[theMKey] === theMValue) {
+				matchedResult.push(eachSection);
+			}
+		}
+		return matchedResult;
+	}
+}
+function matchSComparison(data: any[], theSCOMPARISON: any): any[] {
+	let matchedResult: any[] = [];
+	let theSKey: string = Object.keys(theSCOMPARISON)[0];
+	let inputString: string = theSCOMPARISON[theSKey];
+
+	if (inputString.includes("*")) {
+		const stringOnlyContainsAstrisks = inputString.replace(/[^*]/g, "");
+		const numberOfAstrisksInInputString = stringOnlyContainsAstrisks.length;
+
+		if (numberOfAstrisksInInputString === 2) {
+			matchedResult = matchContainInputString(theSKey, inputString, data);
+			return matchedResult;
+		} else {
+			if (inputString.charAt(0) === "*") {
+				matchedResult = matchEndWithInputString(theSKey, inputString, data);
+				return matchedResult;
+			} else {
+				matchedResult = matchStartWithInputString(theSKey, inputString, data);
+				return matchedResult;
+			}
+		}
+	} else {
+		for (let eachSection of data) {
+			if (eachSection[theSKey] === inputString) {
+				matchedResult.push(eachSection);
+			}
+		}
+		return matchedResult;
+	}
+}
+function matchNegation(data: any[], theNEGATION: any): any[] {
+	let matchedResult: any[] = [];
+	let notMatchedResult: any[] = matchFILTER(data, theNEGATION);
+	for (let eachSection of data) {
+		if (!notMatchedResult.includes(eachSection)) {
+			matchedResult.push(eachSection);
+		}
+	}
+	return matchedResult;
+}
+
+function matchContainInputString(theSKey: string, inputString: string, data: any[]): any[] {
+	let matchedResult: any[] = [];
+	let inputStringWithoutAstrisk: string = inputString.substring(1, inputString.length - 1);
+	for (let eachSection of data) {
+		if (eachSection[theSKey].includes(inputStringWithoutAstrisk)) {
+			matchedResult.push(eachSection);
+		}
+	}
+	return matchedResult;
+}
+function matchEndWithInputString(theSKey: string, inputString: string, data: any[]): any[] {
+	let matchedResult: any[] = [];
+	let inputStringWithoutAstrisk: string = inputString.substring(1);
+	let inputStringWithoutAstriskLength: number = inputStringWithoutAstrisk.length;
+	for (let eachSection of data) {
+		let eachSectionSValue: string = eachSection[theSKey];
+		let eachSectionSValueLength: number = eachSectionSValue.length;
+		if (eachSectionSValueLength >= inputStringWithoutAstriskLength) {
+			let substringStartingIndex: number = eachSectionSValueLength - inputStringWithoutAstriskLength;
+			if (eachSectionSValue.substring(substringStartingIndex) === inputStringWithoutAstrisk) {
+				matchedResult.push(eachSection);
+			}
+		}
+	}
+	return matchedResult;
+}
+function matchStartWithInputString(theSKey: string, inputString: string, data: any[]): any[] {
+	let matchedResult: any[] = [];
+	let inputStringWithoutAstrisk: string = inputString.substring(0, inputString.length - 1);
+	let inputStringWithoutAstriskLength: number = inputStringWithoutAstrisk.length;
+	for (let eachSection of data) {
+		let eachSectionSValue: string = eachSection[theSKey];
+		let eachSectionSValueLength: number = eachSectionSValue.length;
+		if (eachSectionSValueLength >= inputStringWithoutAstriskLength) {
+			if (eachSectionSValue.substring(0, inputStringWithoutAstriskLength) === inputStringWithoutAstrisk) {
+				matchedResult.push(eachSection);
+			}
+		}
+	}
+	return matchedResult;
+}
