3e348402c0b96a6b5d8e92a15d5eafa113d84a51
xevvvw <xevvvw@students.cs.ubc.ca>
2021-11-26 09:23:11 -0800

    finished endpoints

--- src/rest/Server.ts
+++ src/rest/Server.ts
@@ -1,6 +1,9 @@
 import express, {Application, Request, Response} from "express";
 import * as http from "http";
 import cors from "cors";
+import InsightFacade from "../controller/InsightFacade";
+import {InsightDatasetKind, InsightError, NotFoundError} from "../controller/IInsightFacade";
+import fs from "fs";
 
 export default class Server {
 	private readonly port: number;
@@ -18,7 +21,7 @@ export default class Server {
 		// NOTE: you can serve static frontend files in from your express server
 		// by uncommenting the line below. This makes files in ./frontend/public
 		// accessible at http://localhost:<port>/
-		// this.express.use(express.static("./frontend/public"))
+		this.express.use(express.static("./frontend/public"));
 	}
 
 	/**
@@ -85,7 +88,71 @@ export default class Server {
 		this.express.get("/echo/:msg", Server.echo);
 
 		// TODO: your other endpoints should go here
+		this.express.put("/dataset/:id/:kind", Server.addData);
+		this.express.delete("/dataset/:id", Server.removeData);
+		this.express.post("/query", Server.performQuery);
+		this.express.get("/datasets", Server.listData);
+	}
 
+	private static addData(req: Request, res: Response) {
+		console.log(`Server::put(..) - params: ${JSON.stringify(req.params)}`);
+		const rooms = fs.readFileSync("./test/resources/archives/rooms.zip");
+		const courses = fs.readFileSync("./test/resources/archives/courses.zip");
+		const insightFacade = new InsightFacade();
+		let content;
+		let kind = (req.params.kind === "courses") ? InsightDatasetKind.Courses :
+			(req.params.kind === "rooms") ? InsightDatasetKind.Rooms : null;
+		if (kind === InsightDatasetKind.Rooms) {
+			content = rooms.toString("base64");
+		} else if (kind === InsightDatasetKind.Courses) {
+			content = courses.toString("base64");
+		}
+		if (content && kind) {
+			insightFacade.addDataset(req.params.id, content, kind).then((response) => {
+				res.status(200).json({result: response + " added"});
+			}).catch((err) => {
+				res.status(400).json({error: err.message});
+			});
+		} else {
+			let err = new InsightError("not valid request");
+			res.status(400).json({error: err.message});
+		}
+	}
+
+	private static listData(req: Request, res: Response) {
+		console.log(`Server::list(..) - params: ${JSON.stringify(req.params)}`);
+		const insightFacade = new InsightFacade();
+		insightFacade.listDatasets().then((response) => {
+			res.status(200).json({result: response});
+		}).catch((err) => {
+			res.status(400).json({error: err.message});
+		});
+	}
+
+	private static removeData(req: Request, res: Response) {
+		console.trace("Server::delete(..) - params: " + JSON.stringify(req.params));
+		const insightFacade = new InsightFacade();
+		if (insightFacade.datasetIDList.includes(req.params.id)) {
+			insightFacade.removeDataset(req.params.id).then((response) => {
+				res.status(200).json({result: response + " deleted"});
+			}).catch((err) => {
+				res.status(400).json({error: err.message});
+			});
+		} else {
+			let error = new NotFoundError("NotFoundError");
+			res.status(404).json({error: error.message});
+		}
+	}
+
+	private static performQuery(req: Request, res: Response) {
+		console.log("Server::post(..) - params: " + JSON.stringify(req.params));
+		const insightFacade = new InsightFacade();
+		let query = req.body;
+		insightFacade.performQuery(query).then((response) => {
+			res.status(200).json({result: response});
+		}).catch(function (err) {
+			res.status(400).json({result: err.message});
+		});
 	}
 
 	// The next two methods handle the echo service.
